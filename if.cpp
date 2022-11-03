c952c9e63bec9676f4217842e6a360ecaea2fea5
Noname <102451761+SoyUnUber@users.noreply.github.com>
2022-11-03 12:23:14 -0300

    Create if.cpp

---
+++ if.cpp
@@ -0,0 +1,27 @@
+//primer programa c++
+
+#include<iostream>
+
+using namespace std; //indicamos que usamos el estandar de c++, solo flujo de entrada salida como cout
+
+int main(){
+
+int numero, dato = 5;
+
+cout<<"digite un numero";
+cin>>numero; //salga en pantalla
+
+
+if(numero == dato){ //si el numero es igual al dato (5)
+ 
+ cout<<"el numero es 5";
+
+} 
+else{
+	cout<<"el numero es diferente de 5";
+} 
+
+
+
+	return 0; 
+}
