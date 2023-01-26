731057874420937ce6b6ef8f9bfaa8ed06aae26d
Noname <102451761+SoyUnUber@users.noreply.github.com>
2023-01-25 22:51:55 -0300

    Create sumavariables2.cpp

---
+++ nuevo/sumavariables2.cpp
@@ -0,0 +1,16 @@
+#include <iostream> //libreria para escribir en la consola 
+#include <stdlib.h> //libreria para que no se cierre el programa 
+
+int num = 34; //creamos variable llamada NUM que va a valer 34
+int num2 = 5; 
+int suma = num + num2;
+	
+using namespace std; 
+
+
+int main ()
+{
+   cout << suma << "\n\n" << endl; 
+   system ("PAUSE");
+   return 0;
+}
