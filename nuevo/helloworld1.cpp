0f74ee0db5c4eb0ccd9ac7ddfa0b5940f318e1f5
Noname <102451761+SoyUnUber@users.noreply.github.com>
2023-01-25 22:30:58 -0300

    Create helloworld1.cpp

---
+++ nuevo/helloworld1.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h> /* libreria */
+#include <stdlib.h> /*libreria para que no se cierre el programa */
+
+int main() /*funcion */
+{
+   printf /*funcion libreria stdio.h*/ ("hola mundo \n \n"); // \n\n espaciados
+   system("PAUSE"); //funcion de la libreria stdlib, continua ejecutandose el programa hasta que cerremos bash 
+   return 0; 
+}
