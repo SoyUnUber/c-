38dc6114f1eb4ab24dcf55348a08e31af1122948
Noname <102451761+SoyUnUber@users.noreply.github.com>
2023-01-25 23:05:55 -0300

    Create 4-bool.cpp

---
+++ nuevo/4-bool.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdlib.h> 
+	
+using namespace std; 
+
+bool variable = false; //si es falso va a ser 0 el valor de la variable, si es TRUE el valor va ser 1.
+//tipo de variable logico, solamente guarda 0 o 1, verdadero 0 falso
+int main ()
+{ 
+   cout << variable << "\n\n" << endl;
+   system ("PAUSE");
+   return 0;
+}
