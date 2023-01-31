d6bd992820620b01533ad2939da7ccd04426ca12
Noname <102451761+SoyUnUber@users.noreply.github.com>
2023-01-30 21:07:40 -0300

    Create 5-double.cpp

---
+++ nuevo/5-double.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main ()
+{ 
+   double num= 12.45; //double ocupa mas lugar en la memoria, porque puede guardar hasta 10^308 o 10^-308, puede guardar numeros peque;os o numeros grandes. se usa cuando queremos guardar numeros relativamente grandes o peque;os, con muchos decimales, con mayor precision.
+   cout << num<< endl;
+   getch(); 
+   return 0;
+}
